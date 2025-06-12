16272d36b54e4261574b9d9efa03db8080a0ea29
Kazaki-Dev0 <kaique.s.campos@aluno.senai.br>
2025-06-12 17:25:45 -0300

    cronometro atualizado 3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let timerDisplay = document.getElementById('timer');
 let startButton = document.getElementById('start');
 let pauseButton = document.getElementById('pause');
 let resetButton = document.getElementById('reset');
-
+let mudançabtn = document.getElementById('modo');
 // Variáveis para controle do cronômetro
 let seconds = 0;
 let intervalId = null;
@@ -11,8 +11,28 @@ let intervalId = null;
 startButton.addEventListener('click', iniciarTimer);
 pauseButton.addEventListener('click', pauseTimer);
 resetButton.addEventListener('click', resetTimer);
-
-
+mudançabtn.addEventListener('click', mudarModo);
+   
+function mudarModo() {
+    let body = document.body;
+    if (body.classList.contains('dark-mode')) {
+        body.classList.remove('dark-mode');
+        mudançabtn.textContent = 'Modo Escuro';
+        mudançabtn.style.backgroundColor = '#ffffff'; // Cor do botão no modo claro
+        mudançabtn.style.color = '#000000'; // Texto do botão no modo claro
+        body.style.backgroundColor = '#f0f0f0'; // Cor de fundo no modo claro   
+        timer.style.color = '#000'; // Cor do texto do cronômetro no modo claro
+        buttoms.style.backgroundColor = '#444'; // Cor dos botões no modo claro
+    } else {
+        body.classList.add('dark-mode');
+        mudançabtn.textContent = 'Modo Claro';
+        mudançabtn.style.backgroundColor = '#000000'; // Cor do botão no modo escuro
+        mudançabtn.style.color = '#ffffff'; // Texto do botão no modo escuro
+        body.style.backgroundColor = '#121212'; // Cor de fundo no modo escuro
+        timer.style.color = '#fff'; // Cor do texto do cronômetro no modo escuro
+        buttoms.style.backgroundColor = '#f0f8ff';
+    }
+}
 
 // Função para iniciar o cronômetro
 function iniciarTimer() {
